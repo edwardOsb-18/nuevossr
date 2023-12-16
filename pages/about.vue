d26d6a56281e6261d014c41a70043128f4f03c3d
edward <you@gmail.com>
2023-12-16 13:09:58 -0500

    dbkk

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>about</h1>
+  <h1>about22</h1>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda alias odio pariatur, tempora animi fugiat repudiandae culpa commodi. Officia, laboriosam.</p>
 </template>
 
